58554ee668dc2ac2e678e98cbfa0f0c0a6fd1381
VictorIdowu <idsonvictor@gmail.com>
2024-07-24 16:00:27 +0100

    kk

--- src/app/navbar/Navbar.component.ts
+++ src/app/navbar/Navbar.component.ts
@@ -1,12 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class AppNavbar {}
+export class AppNavbar {
+  isLoggedIn: boolean = false;
+  userName: string = 'John Doe';
+
+  items = ['Stone', 'Chair', 'House'];
+}
 
 // "./node_modules/fomantic-ui-css/semantic.min.css",
